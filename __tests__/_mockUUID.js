a9b8dd79fa6f059ff6d847b668b0c32eb01dd6db
mathieudutour <mathieu@dutour.me>
2018-06-11 08:00:43 -0700

    use fs-extra

--- __tests__/_mockUUID.js
+++ __tests__/_mockUUID.js
@@ -3,7 +3,7 @@ jest.mock('uuid/v1')
 
 const uuid = require('uuid/v1')
 uuid
-.mockImplementationOnce(() => '9fa7c9e0-61c3-11e7-9b20-13a8266131bc')
-.mockImplementationOnce(() => '9fa81800-61c3-11e7-9b20-13a8266131bc')
+  .mockImplementationOnce(() => '9fa7c9e0-61c3-11e7-9b20-13a8266131bc')
+  .mockImplementationOnce(() => '9fa81800-61c3-11e7-9b20-13a8266131bc')
 
 module.exports = uuid
